c78f73fa8e215b84aa0190e1b786687c39670954
kalle saas <kalle@easypep.de>
2021-03-10 11:38:48 +0100

    add failing test

--- spec/dummy.rb
+++ spec/dummy.rb
@@ -31,6 +31,7 @@ end
 
 class User < ActiveRecord::Base
   has_many :notes
+  scope :created_before, ->(date) { where('created_at < ?', date) }
 end
 
 class Note < ActiveRecord::Base
@@ -83,7 +84,8 @@ class UsersController < ActionController::Base
   def index
     allowed_fields = [
       :first_name, :last_name, :created_at,
-      :notes_created_at, :notes_quantity
+      :notes_created_at, :notes_quantity,
+      :created_before
     ]
     options = { sort_with_expressions: true }
 
